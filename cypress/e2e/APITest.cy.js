c6989d7fa57a0166310b00b6d019e4d7aab3b025
CheitanB <cheitanb@CheitanBs-MacBook-Pro.local>
2022-12-20 03:08:19 +0530

    comparting the data in response and test data filr

--- cypress/e2e/APITest.cy.js
+++ cypress/e2e/APITest.cy.js
@@ -7,14 +7,17 @@ describe('api test', () => {
 
         cy.get('@listofpages').then((pagesData) => {
             cy.fixture('testData').then((dataFixture) => {
-                expect(dataFixture[0].id).to.deep.equal(7)
+                 expect(dataFixture[0].id).to.deep.equal(7)
                  expect(pagesData.status).to.equal(200)
                  expect(pagesData).to.have.property('body')
+                 cy.expect(pagesData.body.data).to.deep.equal(dataFixture)
                  expect(pagesData.body.data[0].id).to.equal(dataFixture[0].id)
                  expect(pagesData.body.data[0].email).to.equal(dataFixture[0].email)
                  expect(pagesData.body.data[0].first_name).to.equal(dataFixture[0].first_name)
                  expect(pagesData.body.data[0].last_name).to.equal(dataFixture[0].last_name)
                  expect(pagesData.body.data[0].avatar).to.equal(dataFixture[0].avatar)
+               
+                 
                 })
         })
        
